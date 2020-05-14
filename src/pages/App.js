0c15008e81230279971cce3c76ba7d3011e3a4fc
Raphael <paiva.raphaelt@gmail.com>
2020-05-14 20:13:56 +0200

    first commit

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello world !</h1>
+     <Layout />
+    </div>
+  );
+}
+
+export default App;
