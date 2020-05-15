608eee3d6a984dba485cad435589217841554522
Raphael <paiva.raphaelt@gmail.com>
2020-05-16 00:03:28 +0200

    add menu to start the game

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home () {
     return (
         <React.Fragment>
-            <h1>Hello World from Home</h1>
+            <h1>Back to Snake</h1>
+            <Link to="/lvl1">
+                Let's start
+            </Link>
         </React.Fragment>
     )
 }
