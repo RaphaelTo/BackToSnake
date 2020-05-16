0877843361ff4aa91ca42db80c986c0dd4b7a323
Raphael <paiva.raphaelt@gmail.com>
2020-05-16 20:06:11 +0200

    change React.fragment to div

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Home.css';
+
 export default function Home () {
     return (
-        <React.Fragment>
+        <div className="menu">
             <h1>Back to Snake</h1>
             <Link to="/lvl1">
                 Let's start
             </Link>
-        </React.Fragment>
+        </div>
     )
 }
