48e862ae64ea15bdef957fa9da867f0836253940
Raphael <paiva.raphaelt@gmail.com>
2020-05-17 20:15:43 +0200

    add bordergame component

--- src/pages/Lvl1/Lvl1.js
+++ src/pages/Lvl1/Lvl1.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import BorderGame from '../../components/BorderGame';
 
 export default function Lvl1() {
     return (
         <React.Fragment>
-            <h1>Welcome to lvl1</h1>
+            <BorderGame>
+                <h1>Serpent</h1>
+                <h1>Ennemie</h1>
+            </BorderGame>
+            <h1>Level 1</h1>
         </React.Fragment>
     )
 }
