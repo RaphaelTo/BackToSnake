4879d7b358d2a77cbe5a488e331537eaa6c907a0
Raphael <paiva.raphaelt@gmail.com>
2020-05-20 00:54:30 +0200

    add snake component and test features

--- src/pages/Lvl1/Lvl1.js
+++ src/pages/Lvl1/Lvl1.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BorderGame from '../../components/BorderGame';
+import Snake from '../../components/Snake';
+import { moveSnakeWithKeyboard } from '../../features/MoveKeyboard';
 
 export default function Lvl1() {
+    const [move, setMove] = useState({x: 0});
+
+    useEffect(() => {
+        //
+    },[setMove(moveSnakeWithKeyboard)])
+
     return (
-        <React.Fragment>
+        <div className="level" tabIndex="0" onKeyDownCapture={moveSnakeWithKeyboard}>
             <BorderGame>
-                <h1>Serpent</h1>
+                <Snake/>
                 <h1>Ennemie</h1>
             </BorderGame>
             <h1>Level 1</h1>
-        </React.Fragment>
+        </div>
     )
 }
