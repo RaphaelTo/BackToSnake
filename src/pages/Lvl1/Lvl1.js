681080ebaea8ac9ac6bb5606309ba0ea926d4804
Raphael <paiva.raphaelt@gmail.com>
2020-05-20 23:22:55 +0200

    disabled features/Move.. and create method in lvl1

--- src/pages/Lvl1/Lvl1.js
+++ src/pages/Lvl1/Lvl1.js
@@ -1,19 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import BorderGame from '../../components/BorderGame';
 import Snake from '../../components/Snake';
-import { moveSnakeWithKeyboard } from '../../features/MoveKeyboard';
+//import MoveKeyboard from '../../features/MoveKeyboard';
 
 export default function Lvl1() {
     const [move, setMove] = useState({x: 0});
 
-    useEffect(() => {
-        //
-    },[setMove(moveSnakeWithKeyboard)])
+    const moveSnakeWithKeyboard = ({key}) => {
+        if(key === "ArrowUp"){
+           setMove({x : -1});
+        }
+        if(key === "ArrowDown"){
+            setMove({x : 1});
+        }
+        if(key === "ArrowLeft"){
+            setMove({y : -1});
+        }
+        if(key === "ArrowRight"){
+            setMove({y : 1})
+        }
+    }
 
     return (
         <div className="level" tabIndex="0" onKeyDownCapture={moveSnakeWithKeyboard}>
             <BorderGame>
-                <Snake/>
+                <Snake mouvementSnake={move}/>
                 <h1>Ennemie</h1>
             </BorderGame>
             <h1>Level 1</h1>
