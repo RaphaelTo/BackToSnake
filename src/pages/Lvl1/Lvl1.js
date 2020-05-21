ae1234c7a4a3113dd2f36b60a7c1aa57a01cab8a
Raphael <paiva.raphaelt@gmail.com>
2020-05-21 18:21:14 +0200

    add state, effect, but this is beta structure

--- src/pages/Lvl1/Lvl1.js
+++ src/pages/Lvl1/Lvl1.js
@@ -1,15 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BorderGame from '../../components/BorderGame';
 import Snake from '../../components/Snake';
-import { moveSnakeWithKeyboard } from '../../features/MoveKeyboard';
+import PowerUp from '../../components/PowerUp';
+import { moveSnakeWithKeyboard } from '../../features/moveKeyboard';
+import RandomSpawn from '../../features/RandomSpawn';
 
 export default function Lvl1() {
-    const [move, setMove] = useState({x: 0});
+    const [positionSnake, setPositionSnake] = useState({x: 0, y:0});
+    const [positionBorder, setPositionBorder] = useState();
+    const [randPowerUp, setRandPowerUp] = useState({})
+    
+    const getRef = (e) => {
+        const { top, bottom, left, right } = e.current.getBoundingClientRect();
+        setPositionBorder({
+            height : {
+                minHeight: Math.round(top),
+                maxHeight: Math.round(bottom)
+            },
+            width : {
+                minWidth: Math.round(left), 
+                maxWidth: Math.round(right)
+            }
+        })
+    };
+
+    useEffect(() => {
+        if(positionBorder !== undefined){
+            //console.log(positionBorder)
+            const randObject = new RandomSpawn(positionBorder.height, positionBorder.width);
+            const randNumber = randObject.randomSpawn();
+            setRandPowerUp(randNumber);
+        }
+    }, [positionBorder])
 
     return (
-        <div className="level" tabIndex="0" onKeyDownCapture={e => moveSnakeWithKeyboard(e, setMove)}>
-            <BorderGame>
-                <Snake mouvementSnake={move}/>
+        <div className="level" tabIndex="0" onKeyDownCapture={e => moveSnakeWithKeyboard(e, setPositionSnake)}>
+            <BorderGame getRef={getRef}>
+                <Snake positionSnake={positionSnake}/>
+                <PowerUp positionPowerUp={randPowerUp}/>
                 <h1>Ennemie</h1>
             </BorderGame>
             <h1>Level 1</h1>
