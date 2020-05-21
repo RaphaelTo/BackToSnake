f0581d24214b9f9b8ac57b4bb6977217e2919b94
Raphael <paiva.raphaelt@gmail.com>
2020-05-21 18:10:15 +0200

    comment speed, change % to px

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -1,26 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 
-export default function Snake({mouvementSnake}) {
-    const [speed, setSpeed] = useState(1.5);
-    const [xPosition, setXPosition] = useState(50);
-    const [yPosition, setYPosition] = useState(50);
+export default function Snake({positionSnake}) {
+    //const [speed, setSpeed] = useState(0);
+    const [xPosition, setXPosition] = useState(window.innerHeight / 2);
+    const [yPosition, setYPosition] = useState(window.innerWidth / 2);
 
     const BetaSnake = {
         position: 'absolute',
-        left: `${yPosition}%`,
-        top: `${xPosition}%`,
+        left: `${yPosition}px`,
+        top: `${xPosition}px`,
         border: '5px solid white',
         backgroundColor: 'white',
     }
 
     useEffect(() => {
-        if(Object.keys(mouvementSnake)[0] === "x"){
-            setXPosition(mouvementSnake.x + xPosition)
-        }else if(Object.keys(mouvementSnake)[0] === "y"){
-            setYPosition(mouvementSnake.y + yPosition)
+        if(Object.keys(positionSnake)[0] === "x"){
+            setXPosition(positionSnake.x + xPosition)   
+        }else if(Object.keys(positionSnake)[0] === "y"){
+            setYPosition(positionSnake.y + yPosition)
         }
-    }, [mouvementSnake])
+    }, [positionSnake])
 
 
     return (
@@ -31,5 +31,5 @@ export default function Snake({mouvementSnake}) {
 }
 
 Snake.propTypes = {
-    mouvementSnake : PropTypes.object,
+    positionSnake : PropTypes.object,
 }
