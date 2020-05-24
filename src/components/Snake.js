b959c9d31f971f3e82039d058b2511ab36c6bb0a
Raphael <paiva.raphaelt@gmail.com>
2020-05-24 23:08:52 +0200

    export method in useEffect

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -1,33 +1,34 @@
 import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
+import { changePositionCSSSnake } from '../features/moveKeyboard';
 //import SnakeSVG from './SnakeSVG';
 
 export default function Snake({positionSnake}) {
     const [speed] = useState(3);
     const [xPosition, setXPosition] = useState(window.innerHeight / 2);
     const [yPosition, setYPosition] = useState(window.innerWidth / 2);
+    const [eaten, setEeaten] = useState(1)
 
     const BetaSnake = {
         border: '5px solid white',
         position: 'absolute',
         left: `${yPosition}px`,
         top: `${xPosition}px`,
+        width: `${eaten*2}%`
     }
 
+    const X = {
+        xPosition : xPosition,
+        setXPosition: (state) => setXPosition(state)
+    };
+
+    const Y = {
+        yPosition : yPosition,
+        setYPosition: (state) => setYPosition(state)
+    };
+
     useEffect(() => {
-        if(Object.keys(positionSnake)[0] === "x"){
-            if(positionSnake.x === -1){
-                setXPosition((positionSnake.x + xPosition) - speed)
-            }else{
-                setXPosition(positionSnake.x + xPosition + speed)   
-            }
-        }else if(Object.keys(positionSnake)[0] === "y"){
-            if(positionSnake.y === -1){
-                setYPosition((positionSnake.y + yPosition) - speed)
-            }else{
-                setYPosition(positionSnake.y + yPosition + speed)
-            }
-        }
+        changePositionCSSSnake(positionSnake, X, Y, speed);
     }, [positionSnake])
 
 
